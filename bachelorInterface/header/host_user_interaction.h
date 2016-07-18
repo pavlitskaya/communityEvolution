ba5fc8679dd8062d5263f1097fb72f167474b109
Svetlana Pavlitskaya <kurpurniak@gmail.com>
2016-07-18 02:46:36 +0200

    Own interface for own needs.

---
+++ bachelorInterface/header/host_user_interaction.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+
+#include "host_user_interaction_res.h"
+#include <data_source.h>
+
+void interact_with_user();
---
+++ bachelorInterface/source/community_evolution_main.cpp
@@ -0,0 +1,56 @@
+#include "../stdafx.h"
+#include "../header/host_user_interaction.h"
+#include <data_info.h>
+
+#define GPU true
+
+int main()
+{
+    printf("\nBachelor Evolution\n\n");
+	if (DISPLAY_MEMORY)display_device_memory();
+
+    Source source_raw;
+    Source source_snaps;
+    std::vector<int> parameter;
+    parameter.push_back(1);
+    parameter.push_back(0);
+    source_raw.set_source(filename, RAW, EDGES, parameter);
+
+    cout << "Total nodes: " << source_raw.get_total_nodes() << endl;
+    cout << "Snapshot with max nodes: " << source_raw.get_max_nodes() << endl;
+    cout << "Average nodes: " << source_raw.get_avg_nodes() << endl;
+    cout << "Total edges: " << source_raw.get_total_edges() << endl;
+    cout << "Snapshot with max edges: " << source_raw.get_max_edges() << endl;
+    cout << "Average edges: " << source_raw.get_avg_edges() << endl;
+    cout << endl;
+
+    Threshold threshold = Threshold(iterations, choice);
+    uint32_t iterations = 3; // TODO: remove magic
+
+    if (GPU) {
+        Timing::create_time(6);
+        Timing::start_time(6);
+        Timing::start_time_cpu(6);
+
+        algorithm_propinquity(source_raw, source_snaps, 0, 0, threshold, 0, iterations, 0);
+
+        Timing::stop_time(6);
+        Timing::stop_time_cpu(6);
+    } else {
+        Timing::start_time_cpu(7);
+
+        comevohost::algorithm_propinquity(source_raw, source_snaps, 0, 0, host_threshold, 0, iterations, 0);
+
+        Timing::stop_time_cpu(7);
+    }
+
+    cout << "Number of snapshots: " << source_snaps.get_m().size() << endl;
+    cout << "Total number of communities: " << source_snaps.get_total_communities() << endl;
+    cout << "Maximum number of communities: " << source_snaps.get_max_communities() << endl;
+    cout << "Average number of communities: " << source_snaps.get_avg_communities() << endl;
+
+    cout << endl;
+
+	return 0;
+}
+
