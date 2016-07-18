d206ac9ce8b0f96a82475cededed1ff23553a4c0
Svetlana Pavlitskaya <svetlana.pavlitskaya@rwth-aachen.de>
2016-07-18 01:02:42 +0000

    Working bachelor interface

--- bachelorInterface/source/community_evolution_main.cpp
+++ bachelorInterface/source/community_evolution_main.cpp
@@ -1,5 +1,7 @@
 #include "../stdafx.h"
 #include "../header/host_user_interaction.h"
+#include <data_source.h>
+
 #include <data_info.h>
 
 #define GPU true
@@ -9,12 +11,12 @@ int main()
     printf("\nBachelor Evolution\n\n");
 	if (DISPLAY_MEMORY)display_device_memory();
 
-    Source source_raw;
-    Source source_snaps;
-    std::vector<int> parameter;
+    comevo::Source source_raw;
+    comevo::Source source_snaps;
+    std::vector<uint32_t> parameter;
     parameter.push_back(1);
     parameter.push_back(0);
-    source_raw.set_source(filename, RAW, EDGES, parameter);
+    source_raw.set_source(std::string("test.txt"), RAW, EDGES, parameter);
 
     cout << "Total nodes: " << source_raw.get_total_nodes() << endl;
     cout << "Snapshot with max nodes: " << source_raw.get_max_nodes() << endl;
@@ -24,21 +26,20 @@ int main()
     cout << "Average edges: " << source_raw.get_avg_edges() << endl;
     cout << endl;
 
-    Threshold threshold = Threshold(iterations, choice);
     uint32_t iterations = 3; // TODO: remove magic
 
     if (GPU) {
         Timing::create_time(6);
         Timing::start_time(6);
         Timing::start_time_cpu(6);
-
+	Threshold threshold = Threshold(2, 3);
         algorithm_propinquity(source_raw, source_snaps, 0, 0, threshold, 0, iterations, 0);
 
         Timing::stop_time(6);
         Timing::stop_time_cpu(6);
     } else {
         Timing::start_time_cpu(7);
-
+	comevohost::Threshold host_threshold = comevohost::Threshold(2, 3);
         comevohost::algorithm_propinquity(source_raw, source_snaps, 0, 0, host_threshold, 0, iterations, 0);
 
         Timing::stop_time_cpu(7);
